60286ac69fe6bc97bc315ff6b08b2da7ff88786e
Pankaj Kumar <pankajwebdev06@gmail.com>
2023-04-03 03:42:35 +0530

    This is airbnb clone for my project

---
+++ client/src/ImageHandler.js
@@ -0,0 +1,6 @@
+export default function ImageHandler({ src, ...rest }) {
+    src = src && src.includes('https://') ? src : 'http://127.0.0.1:4000/uploads/' + src;
+    return (
+        <img {...rest} src={src} alt={''} />
+    )
+}
---
+++ client/src/PlaceImg.js
@@ -0,0 +1,17 @@
+import ImageHandler from "./ImageHandler";
+
+export default function PlaceImg({ place, index = 0, className = null }) {
+    if (!place.photos?.length) {
+        return '';
+    };
+
+    if (!className) {
+        className = ' object-cover';
+    }
+
+    return (
+        <div>
+            <ImageHandler className={className} src={place.photos[index]} alt="" />
+        </div>
+    )
+}
